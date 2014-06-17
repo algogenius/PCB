08f87e02ec6a441be2a0e78571f552f5334367c9
Christoph Hennig <hanf@parimeo.com>
2014-06-17 23:27:58 +0200

    refining view for cube list and add buttons to add and remove cubes.

--- app/scripts/controllers/cubes.js
+++ app/scripts/controllers/cubes.js
@@ -2,7 +2,6 @@
 
 angular.module('parimeoCubeApp')
     .controller('CubesController', function ($scope) {
-        $scope.greeting = "Christoph";
         $scope.cubes = [
             { x: 35, y: 35, z: 35 },
             { x: 5, y: 5, z: 0 },
