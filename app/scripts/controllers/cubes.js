66c014355ffeac977d6f50cd3e99189ddaa2ffce
Christoph Hennig <hanf@parimeo.com>
2014-06-17 23:01:04 +0200

    making controller more dynamical. now we scope variable to hold some cubes.

--- app/scripts/controllers/cubes.js
+++ app/scripts/controllers/cubes.js
@@ -3,4 +3,9 @@
 angular.module('parimeoCubeApp')
     .controller('CubesController', function ($scope) {
         $scope.greeting = "Christoph";
+        $scope.cubes = [
+            { x: 35, y: 35, z: 35 },
+            { x: 5, y: 5, z: 0 },
+            { x: 10, y: 10, z: 10 }
+        ];
     });
