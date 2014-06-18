1e0a847ea6e8fe0b1edd82a0a7330c7ab278801d
Christoph Hennig <hanf@parimeo.com>
2014-06-18 20:17:03 +0200

    implementing flickr service.

--- app/scripts/services/flickrService.js
+++ app/scripts/services/flickrService.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('parimeoCubeApp')
-    .factory('FlickrService', function ($scope) {
+    .factory('FlickrService', function ($http) {
+        var getPhotosByTagFn = function(tag) {
+            return $http.get("", {
 
+            }).then(function(response) {
+                var array = response.data.photos.photo.map(function(photo){
+                    return "http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg"
+                        .replace("{farm-id}", photo.farm)
+                        .replace("{server-id}", photo.server)
+                        .replace("{id}", photo.id)
+                        .replace("{secret}", photo.secret);
+                });
+                return array;
+            });
+        };
+
+        // public api
+        return {
+            getPhotosByTag: function(tag) {
+                return getPhotosByTagFn(tag);
+            }
+        }
     });
